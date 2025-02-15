03121f58a67b26dc2f67b5ae5fb4f844d8767940
taha <120442688+M-Taha-Amin@users.noreply.github.com>
2025-02-15 21:28:02 +0500

    added new route

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from deployed version</h1>');
+  res.send(
+    '<h1>Hello from deployed version</h1><a href="/new">Go to new page</a>'
+  );
+});
+
+app.get('/new', (req, res) => {
+  res.send('<h1>new route just added</h1>');
 });
 
 app.listen(PORT, () => {
